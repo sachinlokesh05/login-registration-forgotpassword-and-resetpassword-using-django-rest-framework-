9d4a74bb7a23fd20fc51107686ae3359fbdf438e
sachinlokesh05 <sachin.beee.15@acharya.ac.in>
2020-01-28 15:59:58 +0530

    [sachin]added index.html page made by using react

---
+++ project/frontend/src/components/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import ReactDom from "react-dom";
+class App extends Component {
+  render() {
+    return <h1>React App</h1>;
+  }
+}
+ReactDom.render(<App />, document.getElementById("app"));
